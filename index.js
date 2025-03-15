f0edd411c98532d16ab4d2fd92c0e9b32a46860f
Zawadi Kiarie <zawadikiarie@gmail.com>
2025-03-15 20:07:56 +0300

    modify port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
